7841b8eb9128809bca5f6409377e232a2256d8da
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-22 11:46:27 -0300

    first commit

---
+++ src/main/java/com/yujiyamamoto64/pokedex220622/Pokedex220622Application.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.pokedex220622;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pokedex220622Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pokedex220622Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/pokedex220622/Pokedex220622ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.pokedex220622;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pokedex220622ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
